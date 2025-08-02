7d6557e07c777a648fabcf22f4fd5f6c591ba212
Natasha Draper <natasha.draper@entelect.co.za>
2025-08-02 09:44:47 +0200

    JSON + putting a status

--- lib/phx_tutorial_web/controllers/page_controller.ex
+++ lib/phx_tutorial_web/controllers/page_controller.ex
@@ -4,6 +4,9 @@ defmodule PhxTutorialWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    # render(conn, :home, layout: false)
+    conn
+    |> put_status(202)
+    |> render(:home, layout: false)
   end
 end
--- lib/phx_tutorial_web/router.ex
+++ lib/phx_tutorial_web/router.ex
@@ -2,7 +2,7 @@ defmodule PhxTutorialWeb.Router do
   use PhxTutorialWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "json"]
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {PhxTutorialWeb.Layouts, :root}
