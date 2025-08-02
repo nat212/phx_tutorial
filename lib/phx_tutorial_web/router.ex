a1ae517507d3d7a1fbc6428e02a891d27e0b8240
Natasha Draper <natasha.draper@entelect.co.za>
2025-08-02 13:46:43 +0200

    Orders

--- lib/phx_tutorial_web/router.ex
+++ lib/phx_tutorial_web/router.ex
@@ -1,4 +1,5 @@
 defmodule PhxTutorialWeb.Router do
+  alias PhxTutorial.ShoppingCart
   use PhxTutorialWeb, :router
 
   pipeline :browser do
@@ -8,6 +9,8 @@ defmodule PhxTutorialWeb.Router do
     plug :put_root_layout, html: {PhxTutorialWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
+    plug :fetch_current_cart
     plug PhxTutorialWeb.Plugs.Locale, "en"
   end
 
@@ -22,7 +25,15 @@ defmodule PhxTutorialWeb.Router do
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
     get "/redirect_test", PageController, :redirect_test
+
     resources "/products", ProductController
+
+    resources "/cart_items", CartItemController, only: [:create, :delete]
+
+    get "/cart", CartController, :show
+    put "/cart", CartController, :update
+
+    resources "/orders", OrderController, only: [:create, :show]
   end
 
   # Other scopes may use custom stacks.
@@ -44,6 +55,29 @@ defmodule PhxTutorialWeb.Router do
 
       live_dashboard "/dashboard", metrics: PhxTutorialWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+
+      conn
+      |> assign(:current_uuid, new_uuid)
+      |> put_session(:current_uuid, new_uuid)
+    end
+  end
+
+  alias PhxTutorial.ShoppingCart
+
+  defp fetch_current_cart(conn, _opts) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      {:ok, new_cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, new_cart)
     end
   end
 end
